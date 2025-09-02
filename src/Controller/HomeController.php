9feacf1273f6e47a8c0f640db29cd080bb065b96
Gurbala Laurent <69318652+laurentGurbala@users.noreply.github.com>
2025-09-02 13:48:27 +0200

    feat(user): ajout du rôle et gestion du mot de passe

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -19,14 +19,15 @@ class HomeController extends AbstractController
     #[Route("/guests", name: "guests")]
     public function guests(UserRepository $userRepository)
     {
-        $guests = $userRepository->findBy(['admin' => false]);
+        $guests = $userRepository->finGuests();
+
         return $this->render('front/guests.html.twig', [
             'guests' => $guests
         ]);
     }
 
     #[Route("/guest/{id}", name: "guest")]
-    public function guest(int $id,UserRepository $userRepository)
+    public function guest(int $id, UserRepository $userRepository)
     {
         // On récupère le guest via le repository injecté
         $guest = $userRepository->find($id);
@@ -41,11 +42,10 @@ class HomeController extends AbstractController
         MediaRepository $mediaRepository,
         UserRepository $userRepository,
         ?int $id = null
-        )
-    {
+    ) {
         $albums = $albumRepository->findAll();
         $album = $id ? $albumRepository->find($id) : null;
-        $user = $userRepository->findOneBy(['admin' => true]);
+        $user = $userRepository->findAdmin();
 
         $medias = $album ? $mediaRepository->findByAlbum($album) : $mediaRepository->findByUser($user);
 
